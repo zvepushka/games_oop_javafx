a606d33e02cffdd676ea9daa4764b3face2955aa
irina glazunova <zvepushka0@gmail.com>
2022-02-23 20:36:51 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -23,6 +23,7 @@ public class Win {
         }
         return result;
     }
+
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
